be2d75ebf8907bb65ab0cc6f05e358b56024980c
Morgan Giraud <contact@morgangiraud.com>
2022-02-17 13:24:46 +0100

    Add cgol example

---
+++ examples/cgol.py
@@ -0,0 +1,134 @@
+"""Leniax: A simple simulation example.
+
+This is a simple example on how to use Leniax to render a Lenia simulation.
+
+Usage:
+    ``python examples/run.py -cn config_name -cp config_path``
+"""
+import time
+import os
+import logging
+from absl import logging as absl_logging
+from omegaconf import DictConfig
+import hydra
+import jax
+import jax.numpy as jnp
+
+import leniax.utils as leniax_utils
+import leniax.helpers as leniax_helpers
+import leniax.loader as leniax_loader
+from leniax import colormaps as leniax_colormaps
+from leniax.core import register as update_register
+from leniax.kernels import register as kernels_register
+
+# Disable JAX logging https://abseil.io/docs/python/guides/logging
+absl_logging.set_verbosity(absl_logging.ERROR)
+
+
+# We define our own CGOL kernel
+# Leniax convention is [C, H, W] in float32
+# (I know this is not efficient, I'll see later if I can configure dtypes)
+def cgol_2d(*args, **kwargs):
+    return jnp.array([[
+        [1., 1., 1.],
+        [1., 0., 1.],
+        [1., 1., 1.],
+    ]], jnp.float32)
+
+
+kernels_register['cgol_2d'] = cgol_2d
+
+
+# We define a custom cgol function
+def get_state_cgol(
+    rng_key: jax.random.KeyArray,
+    state: jnp.ndarray,
+    field: jnp.ndarray,
+    dt: jnp.ndarray,
+) -> jnp.ndarray:
+    new_state = jnp.array((state == 1) & (field == 2) | (field == 3), dtype=jnp.float32)
+
+    return new_state
+
+
+update_register['get_state_cgol'] = get_state_cgol
+
+###
+# We use hydra to load Leniax configurations.
+# It alloes to do many things among which, we can override configuraiton parameters.
+# For example to render a Lenia in a bigger size:
+# python examples/run.py render_params.world_size='[512, 512]' world_params.scale=4
+###
+cdir = os.path.dirname(os.path.realpath(__file__))
+config_path = os.path.join(cdir, '..', 'conf')
+config_name = "cgol"
+
+
+@hydra.main(config_path=config_path, config_name=config_name)
+def run(omegaConf: DictConfig) -> None:
+    config = leniax_utils.get_container(omegaConf, config_path)
+    leniax_utils.set_log_level(config)
+    leniax_utils.print_config(config)
+
+    save_dir = os.getcwd()  # Hydra change automatically the working directory for each run.
+    leniax_utils.check_dir(save_dir)
+    logging.info(f"Output directory: {save_dir}")
+
+    # We seed the whole python environment.
+    rng_key = leniax_utils.seed_everything(config['run_params']['seed'])
+
+    # We sample some initialize state
+    cells_shape = [1] + config['render_params']['world_size']
+    rng_key, subkey = jax.random.split(rng_key)
+    config['run_params']['init_cells'] = jnp.array(jax.random.randint(subkey, cells_shape, 0, 2), dtype=jnp.float32)
+    # This is the main call which runs and returns data of the simulation
+    # Ony the configuration parameter is mandatory.
+    # In this case:
+    #   - We are using the field "init_cells" as a the simulation initialization
+    #   - We are using JAX jit functions
+    #   - We are not using the fft optimization
+    #   - We won't truncate the computed statistics up to final interesting state (more info on this in the documentation.)
+    # This function returns all the different states, potentials and fields + the statistic dictionnary
+    # All the arrays are of shape [nb_max_iter, N, C, world_dims...]
+    logging.info("Simulation: start.")
+    start_time = time.time()
+    all_cells, _, _, stats_dict = leniax_helpers.init_and_run(
+        rng_key,
+        config,
+        use_init_cells=True,
+        with_jit=True,
+        fft=False,
+        stat_trunc=False,
+    )
+    # In our case, we only ran 1 simulation so N=1
+    all_cells = all_cells[:, 0]
+    total_time = time.time() - start_time
+    nb_iter_done = len(all_cells)
+    logging.info(
+        f"Simulation: stop. {nb_iter_done} states computed in {total_time:.2f} seconds, {nb_iter_done / total_time:.2f} fps."
+    )
+
+    # We then saved the initial and final states.
+    logging.info("Compression: start")
+    start_time = time.time()
+    config['run_params']['init_cells'] = leniax_loader.compress_array(all_cells[0])
+    config['run_params']['cells'] = leniax_loader.compress_array(leniax_utils.center_and_crop(all_cells[-1]))
+    leniax_utils.save_config(save_dir, config)
+    total_time = time.time() - start_time
+    logging.info(f"Compression: stop. Done in {total_time:.2f} seconds.")
+
+    # Finally, we can render our Lenia and other different assets like statistics charts etc.
+    # See the documentation for more information.
+    logging.info("Assets production: start")
+    start_time = time.time()
+    colormaps = [leniax_colormaps.get(cmap_name) for cmap_name in config['render_params']['colormaps']]
+    leniax_helpers.dump_assets(save_dir, config, all_cells, stats_dict, colormaps)
+    for colormap in colormaps:
+        leniax_helpers.dump_frame(save_dir, f'last_frame_cropped_{colormap.name}', all_cells[-1], True, colormap)
+        leniax_helpers.dump_frame(save_dir, f'last_frame_{colormap.name}', all_cells[-1], False, colormap)
+    total_time = time.time() - start_time
+    logging.info(f"Assets production: stop. Done in {total_time:.2f} seconds.")
+
+
+if __name__ == '__main__':
+    run()
--- tests/examples.sh
+++ tests/examples.sh
@@ -13,6 +13,7 @@ TMPDIR=$CURRENT_DIR/hydra_tmp
 set -e  # Exit if any of the commands fail.
 set -x  # Print out commands as they are run.
 
+python examples/cgol.py  render_params.pixel_size=1 render_params.world_size='[64, 64]' run_params.max_run_iter=20 ++other.log_level=30
 python examples/run.py -cp $FIXTURES_DIR -cn orbium-test render_params.pixel_size=1 render_params.size_power2=6 run_params.max_run_iter=20 ++other.log_level=30
 python examples/search_for_init.py -cp $FIXTURES_DIR -cn orbium-test render_params.pixel_size=1 render_params.size_power2=6 run_params.max_run_iter=40 ++run_params.nb_init_search=3 ++other.log_level=30
 python examples/search_for_init_mem_optimized.py -cp $FIXTURES_DIR -cn orbium-test render_params.pixel_size=1 run_params.max_run_iter=40 ++run_params.nb_init_search=3 ++other.log_level=30
@@ -20,6 +21,7 @@ python examples/search_for_mutation.py -cp $FIXTURES_DIR -cn orbium-mut render_p
 python examples/qd_cmame.py -cp $FIXTURES_DIR -cn qd_config-test render_params.pixel_size=1 render_params.size_power2=6 run_params.max_run_iter=20 algo.budget=16 algo.batch_size=2 ++other.log_level=30 ++other.render_bests=False
 python examples/optimization.py -cp $FIXTURES_DIR -cn orbium-test render_params.pixel_size=1 run_params.max_run_iter=64 ++run_params.nb_init_search=2 ++other.log_level=30
 python examples/neural_ca.py -cp $FIXTURES_DIR -cn nca-test render_params.pixel_size=1 run_params.max_run_iter=96 ++run_params.nb_init_search=1 ++run_params.nb_epochs=1 ++other.log_level=30
+python examples/neural_lenia.py -cp $FIXTURES_DIR -cn nca-test render_params.pixel_size=1 run_params.max_run_iter=96 ++run_params.nb_init_search=1 ++run_params.nb_epochs=1 ++other.log_level=30
 
 # Cleanup.
 set +x 
